e56c09ff3afebda31ef0b5721e650ed9321c998c
asherAbecasiss <asherposta@gmail.com>
2023-05-05 15:46:09 +0300

    adding goroutines

--- main.go
+++ main.go
@@ -1,23 +1,105 @@
 package main
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
-	"io"
+	"log"
 	"net/http"
 	"os"
+	"runtime"
 	"strconv"
 	"time"
 )
 
 var PATH string = "/feeds/video"
-var urlPart string = "/api/FeedReceiverManager/UploadChunkFromPath/"
-var serverIp string = "192.168.14.41"
+var urlPart string = "/"
+var serverIp string = "localhost"
+var url = "http://localhost:8081/Start"
 
 type Camera struct {
-	FolderName string
-	MacAdd     string
-	count      *int
+	FolderName string `json:"foldername"`
+	MacAdd     string `json:"macadress"`
+	Count      *int   `json:"count"`
+}
+
+type Status struct {
+	StatusCode int
+	FileName   string
+}
+
+func PostToServer(c []Camera, ch chan<- Status) {
+
+	for _, v := range c {
+
+		if CountFileInFolder(PATH+"/"+v.FolderName) > 3 {
+			tsFileCount := strconv.Itoa(*v.Count)
+			fr := PATH + "/" + v.FolderName + "/" + tsFileCount + ".ts"
+
+			if Exists(fr) {
+				tsFileCount := strconv.Itoa(*v.Count)
+				fr := PATH + "/" + v.FolderName + "/" + tsFileCount + ".ts"
+				// now := time.Now()
+				// formatted2 := now.Format("2006-01-02T15:04:05")
+
+				// url := "http://" + serverIp + urlPart + formatted2 + "/4"
+				// fmt.Println("URL:>", url)
+
+				// bodyText := "{\"filePath\":" + "\"" + PATH + "/" + v.FolderName + "/" + tsFileCount + ".ts" + "\"}"
+
+				// var jsonStr = []byte(bodyText)
+				// req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+				// req.Header.Set("Authorization", v.MacAdd)
+				// req.Header.Set("Content-Type", "application/json")
+				// req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+				// client := &http.Client{}
+				// resp, err := client.Do(req)
+				// if err != nil {
+				// 	panic(err)
+				// }
+				// resp.Body.Close()
+				// fmt.Println("---------------------------------------")
+				// fmt.Println("response Status:", resp.Status, fr)
+				// fmt.Println("response Headers:", resp.Header)
+				// body, _ := io.ReadAll(resp.Body)
+				// fmt.Println("response Body:", string(body))
+				// fmt.Println("---------------------------------------")
+				// fmt.Printf("------------>>>>>>>>>file %s \n", fr)
+				fmt.Printf("------------>>>>>>>>>file %s \n", v.FolderName)
+				*v.Count++
+				os.Remove(fr)
+
+			} else {
+
+				*v.Count++
+				ch <- Status{StatusCode: 0, FileName: fr}
+
+			}
+		} else {
+			ch <- Status{StatusCode: 0, FileName: "number of file less then 3."}
+
+		}
+
+	}
+
+}
+
+func GetParm() []Camera {
+	var CameraList []Camera
+	client := &http.Client{}
+	r, err := client.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Body.Close()
+
+	json.NewDecoder(r.Body).Decode(&CameraList)
+
+	for _, v := range CameraList {
+		v.Count = new(int)
+		*v.Count = 0
+
+	}
+	return CameraList
 }
 
 func main() {
@@ -25,80 +107,36 @@ func main() {
 	// m := make(map[Camera]*int)
 	// var count int = 3
 
-	c1 := Camera{
-		FolderName: "d66cbb46-5c27-4101-8551-fd45bcd2b3cb",
-		MacAdd:     "80647AFFF2A1",
-	}
+	// c1 := Camera{
+	// 	FolderName: "5c20ab46-b5a4-47bb-b3e4-cf6cae4261e4",
+	// 	MacAdd:     "80647AFFF2A1",
+	// }
 
 	// c2 := Camera{
-	// 	FolderName: "313e234c-231b-4ab0-845a-f1f3c412456b",
+	// 	FolderName: "2dabeeca-1f4c-4ad6-abf8-46b53f35f02f",
 	// 	MacAdd:     "80647AFFF299",
 	// }
-	c1.count = new(int)
-	*c1.count = 0
+	// c1.count = new(int)
+	// *c1.count = 0
 	// c2.count = new(int)
 	// *c2.count = 0
+	CameraList := GetParm()
+	log.Printf("Start The Server url %s ", url)
+	fmt.Println(*CameraList[0].Count)
+	ch := make(chan Status)
 
-	var CameraList []Camera
-	CameraList = append(CameraList, c1)
-	// CameraList = append(CameraList, c2)
-
-	// m[c1] = &count
-	// m[c2] = &count
 	for {
 		select {
-		case <-time.After(time.Second):
-			for _, v := range CameraList {
+		case <-time.After(time.Millisecond * 500):
 
-				time.Sleep(time.Second * 1)
-				if CountFileInFolder(PATH+"/"+v.FolderName) > 3 {
-					tsFileCount := strconv.Itoa(*v.count)
-					fr := PATH + "/" + v.FolderName + "/" + tsFileCount + ".ts"
+			go PostToServer(CameraList, ch)
+		// fmt.Println(<-ch)
 
-					if Exists(fr) {
-						tsFileCount := strconv.Itoa(*v.count)
-						fr := PATH + "/" + v.FolderName + "/" + tsFileCount + ".ts"
-						now := time.Now()
-						formatted2 := now.Format("2006-01-02T15:04:05")
-
-						url := "http://" + serverIp + urlPart + formatted2 + "/4"
-						// fmt.Println("URL:>", url)
-
-						bodyText := "{\"filePath\":" + "\"" + PATH + "/" + v.FolderName + "/" + tsFileCount + ".ts" + "\"}"
-
-						var jsonStr = []byte(bodyText)
-						req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-						req.Header.Set("Authorization", v.MacAdd)
-						req.Header.Set("Content-Type", "application/json")
-						req.Header.Set("Accept-Encoding", "gzip, deflate, br")
-						client := &http.Client{}
-						resp, err := client.Do(req)
-						if err != nil {
-							panic(err)
-						}
-						resp.Body.Close()
-						fmt.Println("---------------------------------------")
-						fmt.Println("response Status:", resp.Status, fr)
-						fmt.Println("response Headers:", resp.Header)
-						body, _ := io.ReadAll(resp.Body)
-						fmt.Println("response Body:", string(body))
-						fmt.Println("---------------------------------------")
-
-						*v.count++
-						os.Remove(fr)
-
-					} else {
-						fmt.Printf("------------>>>>>>>>>file not found %s \n", fr)
-						*v.count++
-
-					}
-				} else {
-					time.Sleep(time.Second * 3)
-				}
-
-				// Please add a stop condition !
-			}
+		case d := <-ch:
+			fmt.Println(runtime.NumGoroutine())
+			log.Printf("ch -> %s %d", d.FileName, d.StatusCode)
 		}
+		// fmt.Println(runtime.NumGoroutine())
 
 	}
 
